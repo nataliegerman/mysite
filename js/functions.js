43dc3cd6d8c584f0213c33a5a5d8919065b060e9
natali <aaa>
2016-11-29 01:54:13 +0200

    social buttons are opened in new tab. added function openInNewTab() to functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,10 @@
-
 function openWin() {
     window.open("game.html",'Svin Cat Game', "height=800,width=1010");
+}
+
+function openInNewTab(link){
+     if (link === undefined) {
+        link = "https://www.google.com";
+    }    
+     window.open(link);
 }
